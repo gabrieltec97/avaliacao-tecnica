c283033f57e9f2cf427ac3965bb7de38f404a631
Gabriel Pereira <gabrielphp20@gmail.com>
2025-07-08 12:11:51 -0300

    Parametrização das rotas iniciais de contato

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout.app');
 });
+
+Route::resource('/contatos', ContactController::class);
