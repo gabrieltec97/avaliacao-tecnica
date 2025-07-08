545378fa1dc700bfcb84c7721534035383156279
Gabriel Silva <gabrieldeveloper23@hotmail.com>
2025-07-08 19:08:46 -0300

    Ajuste de rotas - Criação das rotas no arquivo app.
    Criação das rotas no arquivo app.

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -19,10 +19,10 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Meus contatos</a>
+                <a class="nav-link" href="{{ route('contatos.index') }}">Meus contatos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Novo contato</a>
+                <a class="nav-link" href="{{ route('contatos.create') }}">Novo contato</a>
             </li>
         </ul>
     </div>
