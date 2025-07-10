b5705f2294927bd18771ffacf0c463b5645f88e8
Gabriel Pereira <gabrielphp20@gmail.com>
2025-07-10 11:23:35 -0300

    Refatorando funcionalidade de validação de cep.

--- app/Rules/CepValidationRule.php
+++ app/Rules/CepValidationRule.php
@@ -9,37 +9,28 @@ use Illuminate\Support\Facades\Http;
 
 class CepValidationRule implements ValidationRule
 {
-    /**
-     * Run the validation rule.
-     *
-     * @param  \Closure(string, ?string=): \Illuminate\Translation\PotentiallyTranslatedString  $fail
-     */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
         $cep = preg_replace('/[^0-9]/', '', $value);
 
-        // Verifica se o CEP tem 8 dígitos
         if (strlen($cep) !== 8) {
             $fail('O campo :attribute deve conter 8 dígitos.');
             return;
         }
 
-        // Definindo uma chave única para o cache do CEP
+        // Inserindo os valores em cache para evitar multiplas requisições
+        // e otimização de tempo.
         $cacheKey = 'cep_validation_' . $cep;
-
-        // Tenta obter o resultado do cache
         $data = Cache::get($cacheKey);
 
         try {
             $response = Http::get("https://viacep.com.br/ws/{$cep}/json/");
             $data = $response->json();
 
-            // Verifica se a API retornou erro ou não encontrou o CEP
             if (isset($data['erro']) && $data['erro']) {
                 $fail('O CEP informado não é válido ou não foi encontrado.');
             }
         } catch (\Exception $e) {
-            // Trata erros de conexão ou outros problemas
             $fail('Não foi possível validar o CEP no momento. Tente novamente mais tarde.');
         }
     }
